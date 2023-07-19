8fa3370d1984d87205775ef999241822d15853f3
Kujira <64624521+Maimunaki@users.noreply.github.com>
2023-07-19 15:20:28 -0400

    Add files via upload

---
+++ EcoVend Website/code.js
@@ -0,0 +1,6 @@
+function openMainMenu() {
+    document.getElementById("mainMenu").style.width = "400px";
+}
+function closeMainMenu() {
+    document.getElementById("mainMenu").style.width = "0";
+}
