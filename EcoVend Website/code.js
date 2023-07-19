0bfede4dfe14b2202fbcacf46b6679207f059d60
Kujira <64624521+Maimunaki@users.noreply.github.com>
2023-07-19 15:23:27 -0400

    Delete EcoVend Website directory

--- EcoVend Website/code.js
+++
@@ -1,6 +0,0 @@
-function openMainMenu() {
-    document.getElementById("mainMenu").style.width = "400px";
-}
-function closeMainMenu() {
-    document.getElementById("mainMenu").style.width = "0";
-}
